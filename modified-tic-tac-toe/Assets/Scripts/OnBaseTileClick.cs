03be2af14c3cb8425d3f3e963e6e3716ddc63891
Macovei Tudor <tudor_macovei@iCloud.com>
2018-10-29 22:48:55 +0200

    BaseTiles and GameManager added
    The GameManager object, 3 basic sprites were added. The BaseTile objects output to console when clicked.

---
+++ modified-tic-tac-toe/Assets/Scripts/OnBaseTileClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBaseTileClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //How to detect which tile we clicked?
+    void OnMouseDown ()
+    {
+        Debug.Log("You Cliked a Tile");
+    }
+}
