3f5403adaa193f2b857830a4f142cf3e86cda2c2
Macovei Tudor <tudor_macovei@iCloud.com>
2018-10-30 09:12:39 +0200

    Win detection and sprite spawning added

--- modified-tic-tac-toe/Assets/Scripts/GameManager.cs
+++ modified-tic-tac-toe/Assets/Scripts/GameManager.cs
@@ -2,15 +2,136 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    //"Singleton" implementation
+    //The GameManager Class is a singleton, which is good pracice, apparently
+    //I don't understand how this works quite yet, but I'll get there
+    //<BLACK MAGIC>
+    private static GameManager _instance;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("GameManager is running.");
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+                Debug.Log("GameManager initialized");
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    //</BLACK MAGIC>
+
+    private int[,] GameMatrix = new int[3, 3];
+    private int player = new int(); //the player that will place the next piece, 1 = X, 2 = O
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i <= 2; i++)
+            for (int j = 0; j <= 2; j++)
+                GameMatrix[i, j] = 0;
+        player = 1;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void TileToXY(int tile, ref int x, ref int y)
+    {
+        tile = tile - 1;
+        Debug.Log("tile: " + tile + "\n");
+        x = tile % 3;
+        y = tile / 3;
+    }
+
+    //checks if (x, y) is the "center" of a win
+    //returns what player won if it is a win
+    private int WinInXY(int x, int y)
+    {
+        int ok = new int();
+        ok = GameMatrix[x, y];
+
+        if (ok == 0)
+            return 0;
+        //Main diagonal
+        for (int i = -1; i <= 1; i++)
+            if (!(x + i >= 0 && x + i <= 2 && y + i >= 0 && y + i <= 2))
+                ok = 0;
+            else if (GameMatrix[x + i, y + i] != ok)
+                ok = 0;
+        if (ok != 0)
+            return ok;
+        //Secondary diagonal
+        ok = GameMatrix[x, y];
+        for (int i = -1; i <= 1; i++)
+            if (!(x - i >= 0 && x - i <= 2 && y + i >= 0 && y + i <= 2))
+                ok = 0;
+            else if (GameMatrix[x - i, y + i] != ok)
+                ok = 0;
+        if (ok != 0)
+            return ok;
+        //row Y
+        ok = GameMatrix[x, y];
+        for (int i = -1; i <= 1; i++)
+            if (!(x + i >= 0 && x + i <= 2 && y >= 0 && y <= 2))
+                ok = 0;
+            else if (GameMatrix[x + i, y] != ok)
+                ok = 0;
+        if (ok != 0)
+            return ok;
+        //col X
+        ok = GameMatrix[x, y];
+        for (int i = -1; i <= 1; i++)
+            if (!(x >= 0 && x <= 2 && y + i >= 0 && y + i <= 2))
+                ok = 0;
+            else if (GameMatrix[x, y + i] != ok)
+                ok = 0;
+        if (ok != 0)
+            return ok;
+        return 0;
+    }
+
+    //returns 0 as the default
+    //returns 1 if player 1 (X) wins, 2 if player 2 (O) wins
+    private int GameState(int x, int y)
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (WinInXY(i, j) != 0)
+                    return WinInXY(i, j);
+        return 0;
+    }
+
+    public void GameBoardUpdate(GameObject cross, GameObject nought, int tile)
+    {
+        int x = new int();
+        int y = new int();
+        TileToXY(tile, ref x, ref y);
+
+        Debug.Log("x: " + x + "\n y: " + y);
+        if (player == 1)
+        {
+            GameMatrix[x, y] = 1;
+            Object.Instantiate(cross, new Vector3(2.2f * (x - 1), -2.2f * (y - 1), 0), transform.rotation, null);
+            player = 2;
+        }
+        else
+        {
+            GameMatrix[x, y] = 2;
+            Object.Instantiate(nought, new Vector3(2.2f * (x - 1), -2.2f * (y - 1), 0), transform.rotation, null);
+            player = 1;
+        }
+        Debug.Log("GameState: " + GameState(x, y));
+    }
 }
--- modified-tic-tac-toe/Assets/Scripts/OnBaseTileClick.cs
+++ modified-tic-tac-toe/Assets/Scripts/OnBaseTileClick.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnBaseTileClick : MonoBehaviour {
-
+public class OnBaseTileClick : MonoBehaviour
+{
+    public GameObject Cross, Nought;
+    
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
     //How to detect which tile we clicked?
     void OnMouseDown ()
     {
-        Debug.Log("You Cliked a Tile");
+        int tile = new int();
+        tile = this.gameObject.name[9] - '0';
+        GameManager.Instance.GameBoardUpdate(Cross, Nought, tile);
     }
+    //Object.Instantiate(Cross, new Vector3(-2.2f, -2.2f, 0), transform.rotation, null);
+    //spawn sprites in the game manager, when you update the matrix
 }
